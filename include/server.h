b2a3ea2c476ff15a69eaa779e75455f1110ede72
Emin M <idle.cycles26@gmail.com>
2026-04-05 14:09:49 -0400

    create server.h

--- include/server.h
+++ include/server.h
@@ -10,17 +10,17 @@ namespace redis
     class Server
     {
       public:
-        Server(int port);
-        ~Server() noexcept;
+          Server(int port);
+          ~Server() noexcept;
 
-        // listen on the socket + accept client connections
-        void start();
+          // listen on the socket + accept client connections
+          void start();
 
         
         private:
-          sockaddr_in d_addr;
-          int d_server_fd;
-          void spawn(int client_fd);
+            sockaddr_in d_addr;
+            int d_server_fd;
+            void spawn(int client_fd);
     };
 
 } // namespace redis
