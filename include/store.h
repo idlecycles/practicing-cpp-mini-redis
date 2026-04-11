5e3230d2a515f15bdfc741298a97a2a94df6f2b3
Emin M <idle.cycles26@gmail.com>
2026-04-11 10:55:58 -0400

    create store.h

---
+++ include/store.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+namespace redis
+{
+    class Store
+    {
+        public:
+            std::string get(const std::string& key) const;
+            void set(const std::string& key, const std::string& value);
+            void del(const std::string& key);
+            std::vector<std::string> keys() const;
+
+        private:
+            std::unordered_map<std::string, std::string> d_store;
+    };
+
+} // namespace redis
